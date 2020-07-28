0a9f1646158a0aa3f62e76f56a29fbfb32bc3b13
Morgan Jensen <mbjensen822@gmail.com>
2020-07-28 14:39:37 -0600

    New player dialog page now opens when you press the + button. Cannot save yet.

--- app/src/main/java/com/example/eloapp/MainActivity.java
+++ app/src/main/java/com/example/eloapp/MainActivity.java
@@ -7,11 +7,13 @@ import com.google.android.material.snackbar.Snackbar;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+import androidx.fragment.app.FragmentManager;
 
 import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
+    FragmentManager fm;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -24,6 +26,12 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View view) {
                 // show add user fragment
+                fm = getSupportFragmentManager();
+
+                fm.beginTransaction()
+                        .add(android.R.id.content, new NewPlayerDialogFragment())
+                        .addToBackStack(null)
+                        .commit();
             }
         });
     }
