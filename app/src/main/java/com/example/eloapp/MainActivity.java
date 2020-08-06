92030f12b33e3df831ac7636cd5118ce590504c3
Morgan Jensen <mbjensen822@gmail.com>
2020-08-06 14:26:12 -0600

    NOT WORKING. 'Create Match' menu button added but doesn't work.

--- app/src/main/java/com/example/eloapp/MainActivity.java
+++ app/src/main/java/com/example/eloapp/MainActivity.java
@@ -5,10 +5,13 @@ import android.os.Bundle;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.snackbar.Snackbar;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 import androidx.fragment.app.FragmentManager;
 
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
@@ -34,5 +37,27 @@ public class MainActivity extends AppCompatActivity {
                         .commit();
             }
         });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        super.onCreateOptionsMenu(menu);
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        switch(item.getItemId()) {
+            case R.id.action_create_match:
+                fm = getSupportFragmentManager();
+                fm.beginTransaction()
+                        .add(android.R.id.content, new MatchDialogFragment())
+                        .addToBackStack(null)
+                        .commit();
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
     }
 }
