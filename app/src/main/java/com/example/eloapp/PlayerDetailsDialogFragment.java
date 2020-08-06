9ccbfb69cdabc2967dba18c258884876477b53ad
Morgan Jensen <mbjensen822@gmail.com>
2020-08-06 12:27:42 -0600

    delete and 'x' work on player details dialog

--- app/src/main/java/com/example/eloapp/PlayerDetailsDialogFragment.java
+++ app/src/main/java/com/example/eloapp/PlayerDetailsDialogFragment.java
@@ -1,6 +1,8 @@
 package com.example.eloapp;
 
+import android.app.AlertDialog;
 import android.app.Dialog;
+import android.content.DialogInterface;
 import android.os.Bundle;
 
 import androidx.annotation.NonNull;
@@ -37,7 +39,7 @@ public class PlayerDetailsDialogFragment extends DialogFragment {
         root = inflater.inflate(R.layout.fragment_player_details_dialog, container, false);
         txtName = (TextView)root.findViewById(R.id.txtPlayerName_Details);
         txtElo = (TextView)root.findViewById(R.id.txtPlayerElo_Details);
-        toolbar = (Toolbar)root.findViewById(R.id.toolbar);
+        toolbar = (Toolbar)root.findViewById(R.id.toolbar_details);
 
         Bundle bundle = this.getArguments();
 
@@ -82,21 +84,55 @@ public class PlayerDetailsDialogFragment extends DialogFragment {
     @Override
     public void onCreateOptionsMenu(@NonNull Menu menu, @NonNull MenuInflater inflater) {
         menu.clear();
-        getActivity().getMenuInflater().inflate(R.menu.menu_create_dialog, menu);
+        getActivity().getMenuInflater().inflate(R.menu.menu_edit_dialog, menu);
     }
 
     @Override
     public boolean onOptionsItemSelected(@NonNull MenuItem item) {
         switch(item.getItemId()) {
             // click the 'X'
+            case android.R.id.home:
+                dismiss();
+                break;
+            // user clicks delete
+            // use AlertDialog builder
+            // set the PositiveButton click to delete the user from the db.
+            case R.id.menu_delete:
+                AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+                builder.setMessage("Are you sure you want to delete this?")
+                        .setTitle("Delete");
+
+                builder.setPositiveButton("Delete", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        new Thread(new Runnable() {
+                            @Override
+                            public void run() {
+                                AppDatabase.getInstance(getContext())
+                                        .playerDAO()
+                                        .delete(player);
+                            }
+                        }).start();
+                        dismiss();
+                    }
+                });
+
+                builder.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        dismiss();
+                    }
+                });
+
+                AlertDialog alertDialog = builder.create();
+                alertDialog.show();
+                break;
 
             // user clicks on edit
             // create bundle
             // launch the new user dialog fragment and send bundle
 
-            // user clicks delete
-            // use AlertDialog builder
-            // set the PositiveButton click to delete the user from the db.
+
             // ALL DB TRANSACTION NEEDS TO BE DONE IN A THREAD!!!
         }
 
